ce165de89d04de903614fc08bdf29cf4854ea048
v.v.ivanov <wkololo_4ever@mail.ru>
2020-06-15 09:21:38 +0300

    '3.5'

--- 3.5/main.c
+++ 3.5/main.c
@@ -1,23 +1,40 @@
-void main() {
+#include <stdio.h> 
 
+char convertToSymbol(int number);
+void itob(int number, char converted[], unsigned int base);
+
+void main() {
+	char buffer[1000];
+	itob(10, buffer, 16);
 }
 
 void itob(int number, char converted[], unsigned int base) {
 	unsigned int index = 0;
+
 	while (number >=base)
 	{
 		int remainder = number % base;
 		number /= base;
 
-		if (remainder <= 9) {
-			converted[index] = remainder + '0';
-		}
-		else {
-			wh
-			int digit = remainder % 10;
-			remainder/ = 10;
-		}
+		converted[index] = convertToSymbol(remainder);
+		index++;
+	}
 
+	converted[index] = convertToSymbol(number);
+	for (int i = index; i >= 0; i--)
+	{
+		putchar(converted[i]);
+	}
+}
 
+char convertToSymbol(int number) {
+	char bigDigigts[] = "ABCDEF";
+
+	if (number <= 9) {
+		return number + '0';
+	}
+	else {
+		int bigDigigtsIndex = number - 10;
+		return bigDigigts[bigDigigtsIndex];
 	}
 }
